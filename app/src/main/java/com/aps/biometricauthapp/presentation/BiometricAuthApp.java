52411d761aa5f6ec346ab700c5624ddbd7e93c58
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-10-24 12:41:15 -0300

    dependency injection setup

---
+++ app/src/main/java/com/aps/biometricauthapp/presentation/BiometricAuthApp.java
@@ -0,0 +1,8 @@
+package com.aps.biometricauthapp.presentation;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public final class BiometricAuthApp extends Application { }
---
+++ app/src/main/java/com/aps/biometricauthapp/presentation/di/AppModule.java
@@ -0,0 +1,30 @@
+package com.aps.biometricauthapp.presentation.di;
+
+import com.aps.biometricauthapp.data.api.ViaCepService;
+import com.aps.biometricauthapp.presentation.BiometricAuthApp;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.components.SingletonComponent;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+@Module
+@InstallIn(SingletonComponent.class)
+public final class AppModule {
+
+    @Provides
+    @Singleton
+    static ViaCepService provideViaCepService() {
+        return new Retrofit.Builder()
+                .baseUrl("https://viacep.com.br/")
+                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(ViaCepService.class);
+    }
+}
