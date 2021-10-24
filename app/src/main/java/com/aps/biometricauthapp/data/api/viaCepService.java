020239b994742bbb11500f5f931c750dfc3054fa
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-10-24 09:39:29 -0300

    update via cep service

--- app/src/main/java/com/aps/biometricauthapp/data/api/viaCepService.java
+++ app/src/main/java/com/aps/biometricauthapp/data/api/viaCepService.java
@@ -1,2 +1,12 @@
-package com.aps.biometricauthapp.data.api;public interface viaCepService {
+package com.aps.biometricauthapp.data.api;
+
+import com.aps.biometricauthapp.data.model.Address;
+
+import io.reactivex.rxjava3.core.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface viaCepService {
+    @GET("ws/{cep}/json/")
+    Observable<Address> getAddress(@Path("cep") String cep);
 }
