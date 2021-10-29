0ef9820563494a8c23211a0b19725298d8d6ffd6
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-10-29 20:05:28 -0300

    update to user model

--- app/src/main/java/com/aps/biometricauthapp/data/model/User.java
+++ app/src/main/java/com/aps/biometricauthapp/data/model/User.java
@@ -6,30 +6,47 @@ import com.aps.biometricauthapp.util.AccessLevel;
 
 import java.util.Date;
 
+import javax.inject.Inject;
+
 @Entity
 public class User {
+    @Inject
+    Address address;
     private String name;
+    private String email;
     private Date birthday;
-    private Address address;
     private String password;
     private Date loginActivity;
-    private AccessLevel accessLevel;
     private String accessLevelKey;
+    private AccessLevel accessLevel;
 
-    public User(String name,
+    public User() {
+    }
+
+    public User(Address address,
+                String name,
+                String email,
                 Date birthday,
-                Address address,
                 String password,
                 Date loginActivity,
-                AccessLevel accessLevel,
-                String accessLevelKey) {
-        this.name = name;
-        this.birthday = birthday;
+                String accessLevelKey,
+                AccessLevel accessLevel) {
         this.address = address;
+        this.name = name;
+        this.email = email;
+        this.birthday = birthday;
         this.password = password;
         this.loginActivity = loginActivity;
-        this.accessLevel = accessLevel;
         this.accessLevelKey = accessLevelKey;
+        this.accessLevel = accessLevel;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
     }
 
     public String getName() {
@@ -40,20 +57,20 @@ public class User {
         this.name = name;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public Date getBirthday() {
         return birthday;
     }
 
     public void setBirthday(Date birthday) {
         this.birthday = birthday;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
     }
 
     public String getPassword() {
@@ -72,19 +89,19 @@ public class User {
         this.loginActivity = loginActivity;
     }
 
-    public AccessLevel getAccessLevel() {
-        return accessLevel;
-    }
-
-    public void setAccessLevel(AccessLevel accessLevel) {
-        this.accessLevel = accessLevel;
-    }
-
     public String getAccessLevelKey() {
         return accessLevelKey;
     }
 
     public void setAccessLevelKey(String accessLevelKey) {
         this.accessLevelKey = accessLevelKey;
+    }
+
+    public AccessLevel getAccessLevel() {
+        return accessLevel;
+    }
+
+    public void setAccessLevel(AccessLevel accessLevel) {
+        this.accessLevel = accessLevel;
     }
 }
