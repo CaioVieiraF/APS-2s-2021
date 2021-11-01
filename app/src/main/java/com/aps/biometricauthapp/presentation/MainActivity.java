fb0118ab362af3e23374b8e5ef5051d4f7d61294
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-11-01 14:09:26 -0300

    add viewBinding to main activity

--- app/src/main/java/com/aps/biometricauthapp/presentation/MainActivity.java
+++ app/src/main/java/com/aps/biometricauthapp/presentation/MainActivity.java
@@ -1,16 +1,17 @@
 package com.aps.biometricauthapp.presentation;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-import com.aps.biometricauthapp.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.aps.biometricauthapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
