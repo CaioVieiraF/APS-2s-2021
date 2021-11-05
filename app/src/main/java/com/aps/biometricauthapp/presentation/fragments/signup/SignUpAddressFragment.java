02ddf397a176a52fa7b1dc0a6f8665444b4bb7d8
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-11-04 21:11:30 -0300

    add empty text input validation for sign up basic info and address

--- app/src/main/java/com/aps/biometricauthapp/presentation/fragments/signup/SignUpAddressFragment.java
+++ app/src/main/java/com/aps/biometricauthapp/presentation/fragments/signup/SignUpAddressFragment.java
@@ -1,15 +1,20 @@
 package com.aps.biometricauthapp.presentation.fragments.signup;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import com.aps.biometricauthapp.R;
 import com.aps.biometricauthapp.databinding.FragmentSignUpAddressBinding;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Objects;
 
 public class SignUpAddressFragment extends Fragment {
 
@@ -19,10 +24,50 @@ public class SignUpAddressFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentSignUpAddressBinding.inflate(inflater, container, false);
-        binding.continueButton.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.action_signUpAddressFragment_to_signUpAccessLevelFragment));
+        binding.continueButton.setOnClickListener(v -> {
+            if (isTextInputEmpty()) {
+                setErrorOnTextInput();
+            } else {
+                Navigation.findNavController(v).navigate(R.id.action_signUpAddressFragment_to_signUpAccessLevelFragment);
+            }
+        });
         return binding.getRoot();
+    }
+
+    private TextInputLayout[] textInputLayoutList() {
+        return new TextInputLayout[]{
+                binding.textInputAddressComplementLayout,
+                binding.textInputLocationLayout,
+                binding.textInputCepLayout,
+                binding.textInputUfLayout,
+                binding.textInputDistrictLayout,
+                binding.textInputAddressNumberLayout,
+                binding.textInputPublicPlaceLayout
+        };
+    }
+
+    private boolean isTextInputEmpty() {
+        boolean isEmpty = false;
+        TextInputLayout[] textInputLayoutList = textInputLayoutList();
+        for (TextInputLayout textInputLayout : textInputLayoutList) {
+            if (TextUtils.isEmpty(Objects.requireNonNull(textInputLayout.getEditText()).getText())) {
+                isEmpty = true;
+            }
+        }
+        return isEmpty;
+    }
+
+    private void setErrorOnTextInput() {
+        TextInputLayout[] textInputLayoutList = textInputLayoutList();
+        for (TextInputLayout textInputLayout : textInputLayoutList) {
+            if (TextUtils.isEmpty(Objects.requireNonNull(textInputLayout.getEditText()).getText())) {
+                textInputLayout.setError("Campo obrigatório!");
+            } else {
+                textInputLayout.setErrorEnabled(false);
+            }
+        }
     }
 }
--- app/src/main/java/com/aps/biometricauthapp/presentation/fragments/signup/SignUpBasicInfoFragment.java
+++ app/src/main/java/com/aps/biometricauthapp/presentation/fragments/signup/SignUpBasicInfoFragment.java
@@ -1,15 +1,20 @@
 package com.aps.biometricauthapp.presentation.fragments.signup;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import com.aps.biometricauthapp.R;
 import com.aps.biometricauthapp.databinding.FragmentSignUpBasicInfoBinding;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Objects;
 
 public class SignUpBasicInfoFragment extends Fragment {
 
@@ -19,10 +24,49 @@ public class SignUpBasicInfoFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentSignUpBasicInfoBinding.inflate(inflater, container, false);
-        binding.continueButton.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.action_signUpBasicInfoFragment_to_signUpAddressFragment));
+        binding.continueButton.setOnClickListener(v -> {
+            if (isTextInputEmpty()) {
+                setErrorOnTextInput();
+            } else {
+                Navigation.findNavController(v).navigate(R.id.action_signUpBasicInfoFragment_to_signUpAddressFragment);
+            }
+        });
         return binding.getRoot();
+    }
+
+    private TextInputLayout[] textInputLayoutList() {
+        return new TextInputLayout[]{
+                binding.textInputCpfLayout,
+                binding.textInputNameLayout,
+                binding.textInputBirthdayLayout,
+                binding.textInputPhoneLayout,
+                binding.textInputEmailLayout,
+                binding.textInputEmailConfirmationLayout
+        };
+    }
+
+    private boolean isTextInputEmpty() {
+        boolean isEmpty = false;
+        TextInputLayout[] textInputLayoutList = textInputLayoutList();
+        for (TextInputLayout textInputLayout : textInputLayoutList) {
+            if (TextUtils.isEmpty(Objects.requireNonNull(textInputLayout.getEditText()).getText())) {
+                isEmpty = true;
+            }
+        }
+        return isEmpty;
+    }
+
+    private void setErrorOnTextInput() {
+        TextInputLayout[] textInputLayoutList = textInputLayoutList();
+        for (TextInputLayout textInputLayout : textInputLayoutList) {
+            if (TextUtils.isEmpty(Objects.requireNonNull(textInputLayout.getEditText()).getText())) {
+                textInputLayout.setError("Campo obrigatório!");
+            } else {
+                textInputLayout.setErrorEnabled(false);
+            }
+        }
     }
 }
