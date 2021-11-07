d0cfe53446c826e2a13cde3ef4a91e6e77957897
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-11-07 17:56:39 -0300

    override on back pressed to add custom transition animation

--- app/src/main/java/com/aps/biometricauthapp/presentation/activities/HomeActivity.java
+++ app/src/main/java/com/aps/biometricauthapp/presentation/activities/HomeActivity.java
@@ -20,7 +20,14 @@ public class HomeActivity extends AppCompatActivity {
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView2);
+        assert navHostFragment != null;
         NavController navController = navHostFragment.getNavController();
         NavigationUI.setupWithNavController(binding.bottomNavigation, navController);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
 }
