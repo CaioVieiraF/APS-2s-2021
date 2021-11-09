092cc5ff400bd77be0e2168a78c711a65b92a6c5
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-11-09 20:55:08 -0300

    add missing get address function to repository

--- app/src/main/java/com/aps/biometricauthapp/data/repository/AddressRepository.java
+++ app/src/main/java/com/aps/biometricauthapp/data/repository/AddressRepository.java
@@ -1,9 +1,12 @@
 package com.aps.biometricauthapp.data.repository;
 
 import com.aps.biometricauthapp.data.api.ViaCepService;
+import com.aps.biometricauthapp.data.model.Address;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+
+import retrofit2.Call;
 
 @Singleton
 public class AddressRepository {
@@ -13,5 +16,9 @@ public class AddressRepository {
     @Inject
     public AddressRepository(ViaCepService viaCepService) {
         this.viaCepService = viaCepService;
+    }
+
+    public Call<Address> getAddress(String cep) {
+        return viaCepService.getAddress(cep);
     }
 }
