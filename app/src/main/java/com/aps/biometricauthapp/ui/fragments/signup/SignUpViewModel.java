b444fd0e75191185081d9465f504147840659226
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-11-11 20:28:15 -0300

    add public getAddress function to view model

--- app/src/main/java/com/aps/biometricauthapp/ui/fragments/signup/SignUpViewModel.java
+++ app/src/main/java/com/aps/biometricauthapp/ui/fragments/signup/SignUpViewModel.java
@@ -2,11 +2,13 @@ package com.aps.biometricauthapp.ui.fragments.signup;
 
 import androidx.lifecycle.ViewModel;
 
+import com.aps.biometricauthapp.data.model.Address;
 import com.aps.biometricauthapp.data.repository.AddressRepository;
 
 import javax.inject.Inject;
 
 import dagger.hilt.android.lifecycle.HiltViewModel;
+import retrofit2.Call;
 
 @HiltViewModel
 public class SignUpViewModel extends ViewModel {
@@ -16,5 +18,9 @@ public class SignUpViewModel extends ViewModel {
     @Inject
     public SignUpViewModel(AddressRepository repository) {
         this.repository = repository;
+    }
+
+    public Call<Address> getAddress(String cep) {
+        return repository.getAddress(cep);
     }
 }
