c6d4127e2976c38e7f129efb7e74a347a99c3af8
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-11-13 14:29:40 -0300

    provide logging interceptor and okhttp client as dependencies for retrofit

--- app/src/main/java/com/aps/biometricauthapp/di/AppModule.java
+++ app/src/main/java/com/aps/biometricauthapp/di/AppModule.java
@@ -8,6 +8,8 @@ import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
 import dagger.hilt.components.SingletonComponent;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -17,11 +19,27 @@ public final class AppModule {
 
     @Provides
     @Singleton
-    static ViaCepService provideViaCepService() {
+    static ViaCepService provideViaCepService(OkHttpClient client) {
         return new Retrofit.Builder()
                 .baseUrl("https://viacep.com.br/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build()
                 .create(ViaCepService.class);
+    }
+
+    @Provides
+    @Singleton
+    static HttpLoggingInterceptor provideLoggingInterceptor() {
+        return new HttpLoggingInterceptor()
+                .setLevel(HttpLoggingInterceptor.Level.BODY);
+    }
+
+    @Provides
+    @Singleton
+    static OkHttpClient provideOkHttpClient(HttpLoggingInterceptor loggingInterceptor) {
+        return new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build();
     }
 }
