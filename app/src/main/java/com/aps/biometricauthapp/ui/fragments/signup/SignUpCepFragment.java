f223247c27b6582bd5541440c118de4302e8211a
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-11-13 15:29:26 -0300

    add cep validation

--- app/src/main/java/com/aps/biometricauthapp/ui/fragments/signup/SignUpCepFragment.java
+++ app/src/main/java/com/aps/biometricauthapp/ui/fragments/signup/SignUpCepFragment.java
@@ -1,22 +1,32 @@
 package com.aps.biometricauthapp.ui.fragments.signup;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import com.aps.biometricauthapp.R;
+import com.aps.biometricauthapp.data.model.Address;
 import com.aps.biometricauthapp.databinding.FragmentSignUpCepBinding;
 
 import java.util.Objects;
 
+import dagger.hilt.android.AndroidEntryPoint;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+@AndroidEntryPoint
 public class SignUpCepFragment extends Fragment {
 
     private FragmentSignUpCepBinding binding;
+    private SignUpViewModel viewModel;
 
     public SignUpCepFragment() {
     }
@@ -25,10 +35,29 @@ public class SignUpCepFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentSignUpCepBinding.inflate(inflater, container, false);
+        viewModel = new ViewModelProvider(this).get(SignUpViewModel.class);
         binding.continueButton.setOnClickListener(v -> {
-            Bundle bundle = new Bundle();
-            bundle.putString("cep", Objects.requireNonNull(binding.textInputCep.getUnMasked()));
-            Navigation.findNavController(v).navigate(R.id.action_signUpCepFragment_to_signUpAddressFragment, bundle);
+            viewModel.getAddress(binding.textInputCep.getUnMasked()).enqueue(new Callback<Address>() {
+                @Override
+                public void onResponse(Call<Address> call, Response<Address> response) {
+                    try {
+                        if (response.body().getError()) {
+                            binding.textInputCepLayout.setErrorEnabled(true);
+                            binding.textInputCepLayout.setError("CEP inválido!");
+                        }
+                    } catch (NullPointerException e) {
+                        binding.textInputCepLayout.setErrorEnabled(false);
+                        Bundle bundle = new Bundle();
+                        bundle.putString("cep", Objects.requireNonNull(binding.textInputCep.getUnMasked()));
+                        Navigation.findNavController(v).navigate(R.id.action_signUpCepFragment_to_signUpAddressFragment, bundle);
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Address> call, Throwable t) {
+
+                }
+            });
         });
         return binding.getRoot();
     }
