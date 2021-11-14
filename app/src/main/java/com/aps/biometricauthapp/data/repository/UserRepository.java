7b8a8b43a71f02542b7314214f2063be7bb5ed9a
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-11-14 13:48:45 -0300

    update user repository

--- app/src/main/java/com/aps/biometricauthapp/data/repository/UserRepository.java
+++ app/src/main/java/com/aps/biometricauthapp/data/repository/UserRepository.java
@@ -1,7 +1,15 @@
 package com.aps.biometricauthapp.data.repository;
 
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
 import com.aps.biometricauthapp.data.api.ViaCepService;
+import com.aps.biometricauthapp.data.db.UserDao;
 import com.aps.biometricauthapp.data.model.Address;
+import com.aps.biometricauthapp.data.model.User;
+
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -12,13 +20,55 @@ import retrofit2.Call;
 public class UserRepository {
 
     private final ViaCepService viaCepService;
+    private LiveData<List<User>> getAllUsers;
+    private final UserDao userDao;
 
     @Inject
-    public UserRepository(ViaCepService viaCepService) {
+    public UserRepository(ViaCepService viaCepService, UserDao userDao) {
         this.viaCepService = viaCepService;
+        this.userDao = userDao;
+        getAllUsers = userDao.getAllUsers();
     }
 
     public Call<Address> getAddress(String cep) {
         return viaCepService.getAddress(cep);
+    }
+
+    public void insertUser(User user) {
+        new InsertUserAsyncTask(userDao).execute(user);
+    }
+
+    public void updateUser(User user) {
+        new UpdateUserAsyncTask(userDao).execute(user);
+    }
+
+    public LiveData<List<User>> getGetAllUsers() {
+        return getAllUsers;
+    }
+
+    private static class InsertUserAsyncTask extends AsyncTask<User, Void, Void> {
+        private UserDao userDao;
+
+        private InsertUserAsyncTask(UserDao userDao) {
+            this.userDao = userDao;
+        }
+        @Override
+        protected Void doInBackground(User... users) {
+            userDao.insertUser(users[0]);
+            return null;
+        }
+    }
+
+    private static class UpdateUserAsyncTask extends AsyncTask<User, Void, Void> {
+        private UserDao userDao;
+
+        private UpdateUserAsyncTask(UserDao userDao) {
+            this.userDao = userDao;
+        }
+        @Override
+        protected Void doInBackground(User... users) {
+            userDao.updateUser(users[0]);
+            return null;
+        }
     }
 }
