f736a66c003c69e64fd4b22d1f106345ed7b269a
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-11-14 13:54:05 -0300

    update user view model

--- app/src/main/java/com/aps/biometricauthapp/ui/viewmodel/UserViewModel.java
+++ app/src/main/java/com/aps/biometricauthapp/ui/viewmodel/UserViewModel.java
@@ -1,9 +1,13 @@
 package com.aps.biometricauthapp.ui.viewmodel;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.aps.biometricauthapp.data.model.Address;
+import com.aps.biometricauthapp.data.model.User;
 import com.aps.biometricauthapp.data.repository.UserRepository;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -14,13 +18,27 @@ import retrofit2.Call;
 public class UserViewModel extends ViewModel {
 
     private final UserRepository repository;
+    private LiveData<List<User>> getAllUsers;
 
     @Inject
     public UserViewModel(UserRepository repository) {
         this.repository = repository;
+        getAllUsers = repository.getGetAllUsers();
     }
 
     public Call<Address> getAddress(String cep) {
         return repository.getAddress(cep);
+    }
+
+    public void inserUser(User user) {
+        repository.insertUser(user);
+    }
+
+    public void updateUser(User user) {
+        repository.updateUser(user);
+    }
+
+    public LiveData<List<User>> getGetAllUsers() {
+        return getAllUsers;
     }
 }
