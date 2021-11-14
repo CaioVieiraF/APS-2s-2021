06cf43f969ff1f2ee0c991b90a827c1863769fb2
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-11-14 14:51:27 -0300

    add logic for inserting user in database

--- app/src/main/java/com/aps/biometricauthapp/ui/fragments/signup/SignUpSuccessFragment.java
+++ app/src/main/java/com/aps/biometricauthapp/ui/fragments/signup/SignUpSuccessFragment.java
@@ -7,13 +7,23 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import com.aps.biometricauthapp.R;
+import com.aps.biometricauthapp.data.model.Address;
+import com.aps.biometricauthapp.data.model.User;
 import com.aps.biometricauthapp.databinding.FragmentSignUpSuccessBinding;
+import com.aps.biometricauthapp.ui.viewmodel.UserViewModel;
 
+import java.util.ArrayList;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class SignUpSuccessFragment extends Fragment {
-    
+
+    private UserViewModel viewModel;
     private SignUpSuccessFragmentArgs args;
     private FragmentSignUpSuccessBinding binding;
 
@@ -25,9 +35,37 @@ public class SignUpSuccessFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentSignUpSuccessBinding.inflate(inflater, container, false);
         args = SignUpSuccessFragmentArgs.fromBundle(getArguments());
-        binding.loginButton.setOnClickListener(v -> {
-            Navigation.findNavController(v).navigate(R.id.action_signUpSuccessFragment_to_loginFragment);
-        });
+        viewModel = new ViewModelProvider(this).get(UserViewModel.class);
+        insertUser();
+        binding.loginButton.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.action_signUpSuccessFragment_to_loginFragment));
         return binding.getRoot();
+    }
+
+    public void insertUser() {
+        Address address = getAddress();
+        viewModel.insertUser(new User(
+                address,
+                args.getCpf(),
+                args.getName(),
+                args.getEmail(),
+                args.getPhone(),
+                args.getBirthday(),
+                args.getPassword(),
+                new ArrayList<String>(),
+                args.getAccessLevelKey(),
+                args.getAccessLevel(),
+                args.getIsBiometricEnabled())
+        );
+    }
+
+    public Address getAddress() {
+        return new Address(
+                args.getCepMasked(),
+                args.getPublicPlace(),
+                args.getAddressNumberAndComplement(),
+                args.getDistrict(),
+                args.getLocation(),
+                args.getUf()
+        );
     }
 }
