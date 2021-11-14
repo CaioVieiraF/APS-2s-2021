89dfd36b43de67c3350c22dd72420c8f144c73d3
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-11-14 11:47:43 -0300

    add update annotation and update function

--- app/src/main/java/com/aps/biometricauthapp/data/db/UserDao.java
+++ app/src/main/java/com/aps/biometricauthapp/data/db/UserDao.java
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import com.aps.biometricauthapp.data.model.User;
 
@@ -14,6 +15,9 @@ public interface UserDao {
 
     @Insert
     void insertUser(User user);
+
+    @Update
+    void updateUser(User user);
 
     @Query("SELECT * FROM user_table")
     LiveData<List<User>> getAllUsers();
